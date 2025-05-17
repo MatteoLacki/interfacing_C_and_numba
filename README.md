5e1e803832f08cd95aad6a43dbd62da3d2e8769d
Mateusz Lacki <matteo.lacki@gmail.com>
2025-05-17 15:59:59 +0200

    added readme, fixed comment

---
+++ README.md
@@ -0,0 +1,9 @@
+* How to use it?
+
+Requirements:
+* `gcc`
+* `python3` with working `venv` module
+
+Running:
+`make` 
+
--- add_ints.c
+++ add_ints.c
@@ -4,4 +4,4 @@ int64_t sum(int64_t a, int64_t b){
    return a + b;
 }
 
-// gcc add_ints.c -fPIC -shared -o shared_library.so
+// gcc add_ints.c -fPIC -shared -o add_ints.so
